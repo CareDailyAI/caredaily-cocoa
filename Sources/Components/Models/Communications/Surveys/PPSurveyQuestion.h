054c4867062a24dc30cbfd2106f3b4fc3ec57ca3
Destry Teeter <destry.teeter@gmail.com>
2020-07-06 17:11:45 -0700

    > Survey > Realm

--- Sources/Components/Models/Communications/Surveys/PPSurveyQuestion.h
+++ Sources/Components/Models/Communications/Surveys/PPSurveyQuestion.h
@@ -14,7 +14,7 @@ typedef NS_OPTIONS(NSInteger, PPSurveyQuestionId) {
     PPSurveyQuestionIdNone = -1
 };
 
-@interface PPSurveyQuestion: NSObject
+@interface PPSurveyQuestion: RLMObject
 
 @property (nonatomic) PPSurveyQuestionId questionId;
 @property (nonatomic, strong) NSString * _Nullable key;
@@ -35,3 +35,5 @@ typedef NS_OPTIONS(NSInteger, PPSurveyQuestionId) {
 @end
 
 NS_ASSUME_NONNULL_END
+
+RLM_ARRAY_TYPE(PPSurveyQuestion);
