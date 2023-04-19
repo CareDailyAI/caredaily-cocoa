c062a706b02fc566d338fecf53e44ba0e41cfcf6
Destry Teeter <destry.teeter@gmail.com>
2023-04-19 10:12:42 -0700

    verbosity

--- build_framework.sh
+++ build_framework.sh
@@ -4,7 +4,7 @@
 if [ -z ${CI_PRODUCT_PLATFORM+x} ]; then PLATFORMS="ios,watchOS"; else PLATFORMS=${CI_PRODUCT_PLATFORM}; fi
 
 # Install Carthage
-carthage update --platform $PLATFORMS --use-xcframeworks --cache-builds
+carthage update --platform $PLATFORMS --use-xcframeworks --cache-builds --verbose
 
 cd Demo
 
